125e89c06ce1824ac97b0d58ce3bf702f0512921
BollareddyPranoyRaj1 <bollareddypranoyraj@gmail.com>
2026-04-11 23:41:47 +0530

    Mark generic estimates as manual source

--- backend/src/services/estimateEngine.js
+++ backend/src/services/estimateEngine.js
@@ -200,6 +200,7 @@ function estimateResale(input) {
     maxPrice,
     depreciationPercent,
     resaleScore,
+    estimateSource: 'manual',
     keyFactors: [
       `Brand: ${resolvedBrandLabel}`,
       `Model: ${resolvedModelLabel}`,
